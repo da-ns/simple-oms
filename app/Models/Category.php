46411b1ebf53d8f6c599811415373f68216f90f6
da-ns <davydova.n.s@ya.ru>
2025-03-06 13:13:57 +0300

    add category model and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+class Category
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+}
